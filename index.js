65902dc346a48dd0c07db78ca1e88ded1c34c8af
Matheus Oliveira <matheus.oliveirav@gmail.com>
2019-11-11 20:06:24 -0300

    Adding View

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-
+const path = require('path')
 const port = process.env.PORT || 3000
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
