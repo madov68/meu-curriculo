75048c66eab0d5a3b4a25506e88f2c145814ec27
Matheus Oliveira <matheus.oliveirav@gmail.com>
2019-11-11 20:13:25 -0300

    Config public Folder

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const port = process.env.PORT || 3000
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', async(request, response) =>{
     response.render('home')
